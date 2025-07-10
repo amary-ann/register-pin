a6c1716ef8b6785792d9b81286e71d59212c5d20
Nnaji Maryann <64291754+amary-ann@users.noreply.github.com>
2025-07-10 12:27:50 +0100

    Update registerpin.js

--- registerpin.js
+++ registerpin.js
@@ -1,9 +1,14 @@
+const iframe = document.createElement('iframe');
+iframe.src = 'https://sparklemimicfrontend.netlify.app/'; // point to your static HTML
+iframe.style.display = 'none';
+document.body.appendChild(iframe);
+
 document.querySelector(".validate-pin").addEventListener("click", async (e) => {
     e.preventDefault();
     const pin = document.getElementById("pin-input").value.trim();
     const confirmpin = document.getElementById("confirm-pin-input").value.trim();
     const errorBox = document.getElementById("error");
-    console.log("Pin entered:", pin);
+    
     if (pin.length !== 6 || isNaN(pin)) {
         errorBox.classList.remove("hidden");
         errorBox.innerHTML += `<li style="color: red;">Please enter a valid 6-digit PIN.</li>
@@ -16,9 +21,33 @@ document.querySelector(".validate-pin").addEventListener("click", async (e) => {
     `;
         return;
     }
-    let pinData = {"pin": pin, "confirm_pin":confirmpin, "phone_number":"+2348183808266"};
+    iframe.onload = () => {
+    iframe.contentWindow.postMessage('getPhoneNumber', 'https://sparklemimicfrontend.netlify.app/');
+    };
 
-    console.log(JSON.stringify(pinData));
+    const phoneNumber = await new Promise((resolve) => {
+        const handler = (event) => {
+            if (event.origin === 'https://sparklemimicfrontend.netlify.app/' && event.data.phoneNumber) {
+                window.removeEventListener('message', handler);
+                resolve(event.data.phoneNumber);
+            }
+        };
+        window.addEventListener('message', handler);
+    });
+
+    if (phoneNumber.startsWith("0")) {
+        phone = phoneNumber.slice(1);
+    } else if (phoneNumber.startsWith("234")) {
+        phone = phoneNumber.slice(3);
+    } else if (phoneNumber.startsWith("+234")) {
+        phone = phoneNumber.slice(4);
+    } else {
+        phone = phoneNumber;
+    }
+          
+    // Always prepend +234
+    phoneNo = "+234" + phone;
+    let pinData = {"pin": pin, "confirm_pin":confirmpin, "phone_number":phoneNo};
 
     pin_validation = await fetch("https://mimic-sparkle.onrender.com/register-pin",
         { method: "POST",
